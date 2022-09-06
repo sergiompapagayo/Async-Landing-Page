b45fa6740c825c2b343780eda794cf9d57f89032
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-09-05 22:53:03 -0500

    index.js has been updated

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("Hello, World!");
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCtvu0f5wRLBRYISBw_3NDsg&part=snippet%2Cid&order=date&maxResults=9';
+
+const $content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '61afbe6cecmsh03090255213ecefp18adb1jsnc587431ea638',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlAPI) {
+  const response = await fetch(urlAPI, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view =
+    `
+    ${videos.items.map(video =>
+      `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+      `).slice(0,8).join('')}
+    `;
+    $content.innerHTML = view;
+  } catch(error) {
+    console.log(error);
+    alert(error);
+  }
+})();
